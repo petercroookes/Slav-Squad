47f14dc958ed26a764df87c77595a376ac319108
Emile Czernuszka <etpc2@cam.ac.uk>
2022-02-03 21:59:17 +0000

    Task 1F

---
+++ Task1F.py
@@ -0,0 +1,11 @@
+from floodsystem.station import inconsistent_typical_range_stations
+from floodsystem.stationdata import build_station_list
+def run():
+    stations = build_station_list()
+    data_list_names = [station.name for station in inconsistent_typical_range_stations(stations)]
+    #data_list_names.sort()
+    print(data_list_names.sort())
+
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    run()
