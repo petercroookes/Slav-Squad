77a73838c5a01cadd7dc4d235404fd2aabc31106
Peter Crookes <pc693@cam.ac.uk>
2022-02-25 01:45:30 +0000

    bla

--- Task2B.py
+++ Task2B.py
@@ -8,7 +8,8 @@ def run():
     update_water_levels(stations)
     tol = 0.8
     list = stations_level_over_threshold(stations,tol)
-    print(list)
+    name_and_level = [list[0].name, list[1]]
+    print(name_and_level)
 
 if __name__ == "__main__":
     print("\n*** Task 2B: CUED Part IA Flood Warning System ***")
